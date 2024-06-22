c65b72f57e98a48e1d2d455b0d86ed5bac52e74f
Rudy-I28 <rivan.jamjachi@gmail.com>
2024-06-22 00:21:23 -0500

    Curso 1 - Desafio 4: Alura Latam
    Secuencias de ejercicios del desafío 4

---
+++ app.js
@@ -0,0 +1,94 @@
+/*
+//Ejercicio 1
+console.log('¡Bienvenido a mi canal!');
+*/
+/*
+//Ejercicio 2
+let nombre = 'Ivan';
+console.log(`¡Hola, ${nombre}!`);
+*/
+/*
+//Ejercicio 3
+let nombre = 'Ivan';
+alert(`¡Hola, ${nombre}!`);
+*/
+/*
+//Ejercicio 4
+let lenguajeProgramacion = prompt('¿Cúal es el lenguaje de programación que más te gusta?');
+console.log(lenguajeProgramacion);
+*/
+/*
+//Ejercicio 5
+let valor1 = 23;
+let valor2 = 19;
+
+let resultado = valor1 + valor2;
+console.log(`La suma de ${valor1} y ${valor2} es igual a ${resultado}`);
+*/
+/*
+//Ejercicio 6
+let valor1 = 37;
+let valor2 = 19;
+
+let resultado = valor1 - valor2;
+console.log(`La diferencia entre ${valor1} y ${valor2} es igual a ${resultado}`);
+*/
+/*
+//Ejercicio 7
+let edad = prompt('¿Podría ingresar su edad?');
+
+if(edad >= 18){
+    console.log('¡Usted es mayor de edad!');
+} else{
+    console.log('¡Usted aún no es mayor de edad!');
+}
+*/
+/*
+//Ejercicio 8
+let numero = prompt('Ingrese un valor  para verificar si es negativo o positivo');
+
+if( numero > 0){
+    console.log('El numero ingresado es positivo');
+} else if( numero == 0){
+    console.log('El numero ingresado es cero');
+} else{
+    console.log('El numero ingresado es negativo');
+}
+*/
+/*
+//Ejercicio 9
+let contador = 1;
+
+while(contador <= 10){
+    console.log(`* ${contador}`);
+    contador ++;
+}
+*/
+/*
+//Ejercicio 10
+
+let nota = prompt('Ingrese una nota que este entre 0 - 10');
+
+if( nota >= 7 && nota <= 10){
+    console.log('¡Aprobado!');
+} else if( nota >0 && nota < 7){
+    console.log('¡Desaprobado!');
+} else{
+    console.log('¡La nota ingresada no es válida!');
+}
+*/
+/*
+//Ejercicio 11
+let numero = Math.random();
+console.log(numero);
+*/
+/*
+//Ejercicio 12
+let numero = Math.floor(Math.random()*10)+1;
+console.log(numero);
+*/
+/*
+//Ejercicio 13
+let numero = Math.floor(Math.random()*1000)+1;
+console.log(numero);
+*/
